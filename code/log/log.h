cbe7e04a8f77d78597465866915e582cb08f6213
line-6 <3118525052@qq.com>
2025-12-14 15:35:34 +0800

    FINISH Step2 src code

--- code/log/log.h
+++ code/log/log.h
@@ -1,9 +1,23 @@
 #pragma once
 
+#include <mutex>
+#include <string>
+#include <thread>
+#include <sys/time.h>
+#include <string.h>
+#include <stdarg.h>           // vastart va_end
+#include <assert.h>
+#include <sys/stat.h>         //mkdir
+#include "blockqueue.h"
+#include "../buffer/buffer.h"
+
 class Log {
 private:
     Log();
     virtual ~Log();
+
+    void AppendLogLevelTitle(int level);
+    void AsyncWrite();
 
 public:
     void init(int level, const char* path = "./log",
@@ -11,6 +25,51 @@ public:
                 int maxQueueCapacity = 1024);
 
     static Log* Instance();
+    static void FlushLogThread();
 
+    void write(int level, const char* format, ...);
+    void flush();
 
+    int GetLevel();
+    void SetLevel(int level);
+    bool IsOpen() {return isOpen_;}
+
+private:
+    static const int LOG_PATH_LEN = 256;
+    static const int LOG_NAME_LEN = 256;
+    static const int MAX_LINES = 50000;
+
+    const char* path_;
+    const char* suffix_;
+
+    int MAX_LINES_;
+
+    int lineCount_;
+    int toDay_;
+
+    bool isOpen_;
+
+    Buffer buff_;   
+    int level_;
+    bool isAsync_;
+
+    FILE* fp_;
+
+    std::unique_ptr<BlockDeque<std::string>> deque_;
+    std::unique_ptr<std::thread> writeThread_;
+    std::mutex mtx_;
 }; 
+
+#define LOG_BASE(level, format, ...) \
+    do {\
+        Log* log = Log::Instance();\
+        if (log->IsOpen() && log->GetLevel() <= level) {\
+            log->write(level, format, ##__VA_ARGS__); \
+            log->flush();\
+        }\
+    } while(0);
+
+#define LOG_DEBUG(format, ...) do {LOG_BASE(0, format, ##__VA_ARGS__)} while(0);
+#define LOG_INFO(format, ...) do {LOG_BASE(1, format, ##__VA_ARGS__)} while(0);
+#define LOG_WARN(format, ...) do {LOG_BASE(2, format, ##__VA_ARGS__)} while(0);
+#define LOG_ERROR(format, ...) do {LOG_BASE(3, format, ##__VA_ARGS__)} while(0);
